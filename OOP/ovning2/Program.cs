cd72fbb72a379f348064a37536d2a3a5db2e8b8e
Filip Bengs <filip.bengs@netikka.fi>
2021-02-10 20:04:26 +0200

    changed gitignore to include files from cpp course

--- OOP/ovning2/Program.cs
+++ OOP/ovning2/Program.cs
@@ -1,52 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ovning2
 {
-    class Program
+    partial class Program
     {
-        public static bool checkPoints(short points) {
-            if (points >= 0 && points <= 30) return true;
-            else return false;
-        }
-
-        public static short calculateGrade(short points) {
-            if (points > 27) return 5;
-            return Convert.ToInt16((0.25 * points) - 1.5);
-        }
-
-        public static string gradeComment(short grade) {
-            string output;
-            switch (grade)
-            {
-                case 1:
-                case 2:
-                    output = "You scored a " + grade + ", that's ok\n";
-                    break;
-                case 3:
-                case 4:
-                    output = "You scored a " + grade + ", nice job :)\n";
-                    break;
-                case 5:
-                    output = "You scored a 5, that's great :D\n";
-                    break;
-                default:
-                    output = "You scored a 0, nice job :E\n";
-                    break;
-            }
-            return output;
-        }
-
         static void Main()
         {
-            short studentPoints, grade;
+            List<short> list = new List<short>();
+            short input;
 
-            do {
-                Console.WriteLine("Enter students points (should be between 0 and 30): ");
-                studentPoints = Convert.ToInt16(Console.ReadLine());
-            } while (!checkPoints(studentPoints));
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Enter students points (should be between 0 and 30): ");
+                    input = Convert.ToInt16(Console.ReadLine());
+                } while (!checkPoints(input));
+                list.Add(calculateGrade(input));
+                Console.WriteLine("To keep entering press [Any button], to stop entering press [ESC]");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
 
-            grade = calculateGrade(studentPoints);
-            Console.WriteLine(gradeComment(grade));
+            short[] studentPoints = list.ToArray();
+            renderFrequency(studentPoints);
         }
     }
 }
